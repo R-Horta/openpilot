9c83b4eb81cece872ef8cf5069bdf1fb0c8b23d0
R-Horta <103232670+R-Horta@users.noreply.github.com>
2022-05-17 18:04:32 -0300

    adjustments to CAMERA_OFFSET and PATH_OFFSET

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -17,8 +17,8 @@ CameraOffset = int(params.get("CameraOffset", encoding='utf8'))
 # print("User defined Camera Offset value in CM is:", CameraOffset)
 # duplicated camera_offset factor (old value after "/" 100)
 if EON:
-  CAMERA_OFFSET = 0.16 + CameraOffset / 50 #left lane alignment? (old value = 0.15)
-  PATH_OFFSET = 0.08 + CameraOffset / 50 #left lane alignment? (old value = 0.15)
+  CAMERA_OFFSET = 0.14 + CameraOffset / 50 #left lane alignment? (old value = 0.16)
+  PATH_OFFSET = 0.03 + CameraOffset / 50 #left lane alignment? (old value = 0.08)
 elif TICI:
   CAMERA_OFFSET = -0.04 + CameraOffset / 50
   PATH_OFFSET = -0.04 + CameraOffset / 50
